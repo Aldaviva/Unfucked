4b384b127bb2c727fa7ec225a07278f44b0fca43
Ben Hutchison <ben@aldaviva.com>
2026-04-05 11:16:15 -0700

    Added specialized singleton collection classes for dictionaries and sets

--- Unfucked/Singletons.cs
+++ Unfucked/Singletons.cs
@@ -1,4 +1,4 @@
-using System.Collections.ObjectModel;
+using System.Collections;
 
 namespace Unfucked;
 
@@ -17,7 +17,7 @@ public static class Singletons {
         /// <param name="key">single key</param>
         /// <param name="value">single value</param>
         /// <returns>A read-only dictionary with the one specified <paramref name="key"/> and <paramref name="value"/>.</returns>
-        public static IReadOnlyDictionary<K, V> Singleton(K key, V value) => new ReadOnlyDictionary<K, V>(new Dictionary<K, V>(1) { { key, value } });
+        public static IReadOnlyDictionary<K, V> Singleton(K key, V value, IEqualityComparer<K>? keyComparer = null) => new ReadOnlySingletonDictionary<K, V>(key, value, keyComparer);
 
     }
 
@@ -30,7 +30,7 @@ public static class Singletons {
         /// <param name="item">single item</param>
         /// <param name="comparer">optional comparer used for item equality, or <c>null</c> to use the default <see cref="EqualityComparer{T}"/> for <typeparamref name="T"/></param>
         /// <returns>A set with the one specified <paramref name="item"/></returns>
-        public static ISet<T> Singleton(T item, IEqualityComparer<T>? comparer = null) => new HashSet<T>([item], comparer);
+        public static IReadOnlySingletonSet<T> Singleton(T item, IEqualityComparer<T>? comparer = null) => new ReadOnlySingletonSet<T>(item, comparer);
 
     }
 
@@ -43,10 +43,122 @@ public static class Singletons {
     /// <returns>An enumerable with the one specified key-value pair</returns>
     public static IEnumerable<KeyValuePair<K, V>> KeyValues<K, V>(this (K, V) keyValuePair) => [new(keyValuePair.Item1, keyValuePair.Item2)];
 
-}
+    public interface IReadOnlySingletonSet<T>:
+#if NET5_0_OR_GREATER
+        IReadOnlySet<T>,
+#endif
+        ISet<T>;
 
-/// <inheritdoc cref="Singletons" />
-public static class Singletons2 {
+    private sealed class ReadOnlySingletonSet<T>(T singleton, IEqualityComparer<T>? comparer): IReadOnlySingletonSet<T> {
+
+        private readonly IEqualityComparer<T> comparer = comparer ?? EqualityComparer<T>.Default;
+
+        public int Count => 1;
+        public bool IsReadOnly => true;
+
+        public IEnumerator<T> GetEnumerator() => new SingletonEnumerator<T>(singleton);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool IsProperSupersetOf(IEnumerable<T> other) => !other.Any();
+        public bool IsSubsetOf(IEnumerable<T> other) => other.Contains(singleton, comparer);
+        public bool IsSupersetOf(IEnumerable<T> other) => other.All(item => comparer.Equals(singleton, item));
+        public bool Overlaps(IEnumerable<T> other) => IsSubsetOf(other);
+
+        public bool IsProperSubsetOf(IEnumerable<T> other) {
+            bool foundItem = false, foundDifferentItem = false;
+            foreach (T otherItem in other) {
+                bool isItem = comparer.Equals(singleton, otherItem);
+                foundItem          |= isItem;
+                foundDifferentItem |= !isItem;
+                if (foundItem && foundDifferentItem) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool SetEquals(IEnumerable<T> other) {
+            bool empty = true;
+            foreach (T otherItem in other) {
+                empty = false;
+                if (!comparer.Equals(singleton, otherItem)) {
+                    return false;
+                }
+            }
+            return !empty;
+        }
+
+        public bool Contains(T item) => comparer.Equals(singleton, item);
+        public void CopyTo(T[] array, int arrayIndex) => array[arrayIndex] = singleton;
+
+        void ICollection<T>.Add(T item) => throw new NotSupportedException();
+        public void ExceptWith(IEnumerable<T> other) => throw new NotSupportedException();
+        public void IntersectWith(IEnumerable<T> other) => throw new NotSupportedException();
+        public void SymmetricExceptWith(IEnumerable<T> other) => throw new NotSupportedException();
+        public void UnionWith(IEnumerable<T> other) => throw new NotSupportedException();
+        bool ISet<T>.Add(T item) => throw new NotSupportedException();
+        public void Clear() => throw new NotSupportedException();
+        public bool Remove(T item) => throw new NotSupportedException();
+
+    }
+
+    private sealed class SingletonEnumerator<T>(T singleton): IEnumerator<T> {
+
+        private int position;
+
+        public T Current => position == 1 ? singleton : default!;
+        object? IEnumerator.Current => Current;
+
+        public bool MoveNext() => 0 == Interlocked.CompareExchange(ref position, 1, 0);
+
+        public void Reset() => throw new NotSupportedException();
+        public void Dispose() {}
+
+    }
+
+    internal sealed class ReadOnlySingletonDictionary<K, V>(K singletonKey, V singletonValue, IEqualityComparer<K>? keyComparer): IReadOnlyDictionary<K, V>, IDictionary<K, V> where K: notnull {
+
+        private readonly KeyValuePair<K, V>   pair        = new(singletonKey, singletonValue);
+        private readonly IEqualityComparer<K> keyComparer = keyComparer ?? EqualityComparer<K>.Default;
+
+        public int Count => 1;
+        public bool IsReadOnly => true;
+
+        /// <inheritdoc cref="IReadOnlyDictionary{TKey,TValue}.this" />
+        public V this[K key] {
+            get => keyComparer.Equals(singletonKey, key) ? singletonValue : throw new KeyNotFoundException();
+            set => throw new NotSupportedException();
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator() => new SingletonEnumerator<KeyValuePair<K, V>>(pair);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public ICollection<K> Keys => [singletonKey];
+        public ICollection<V> Values => [singletonValue];
+        IEnumerable<K> IReadOnlyDictionary<K, V>.Keys => Keys;
+        IEnumerable<V> IReadOnlyDictionary<K, V>.Values => Values;
+
+        public bool Contains(KeyValuePair<K, V> item) => ContainsKey(item.Key) && singletonValue is {} value ? value.Equals(item.Value) : item.Value is null;
+        public bool ContainsKey(K key) => keyComparer.Equals(singletonKey, key);
+        public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex) => array[arrayIndex] = pair;
+
+        public bool TryGetValue(K key,
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+                                [MaybeNullWhen(false)]
+#endif
+                                out V value) {
+            bool isFound = keyComparer.Equals(key);
+            value = isFound ? singletonValue : default!;
+            return isFound;
+        }
+
+        public void Add(K key, V value) => throw new NotSupportedException();
+        public void Add(KeyValuePair<K, V> item) => throw new NotSupportedException();
+        public void Clear() => throw new NotSupportedException();
+        public bool Remove(KeyValuePair<K, V> item) => throw new NotSupportedException();
+        public bool Remove(K key) => throw new NotSupportedException();
+
+    }
 
     /// <typeparam name="K">key type</typeparam>
     /// <typeparam name="V">value type</typeparam>
@@ -59,6 +171,26 @@ public static class Singletons2 {
         /// <param name="value">single value</param>
         /// <returns>An enumerable with the one specified <paramref name="key"/> and <paramref name="value"/> pair.</returns>
         public static IEnumerable<KeyValuePair<K, V>> Singleton(K key, V value) => [new(key, value)];
+
+    }
+
+}
+
+/// <inheritdoc cref="Singletons" />
+public static class Singletons2 {
+
+    extension(IDictionary) {
+
+        /// <summary>
+        /// Create a read-only singleton dictionary
+        /// </summary>
+        /// <typeparam name="K">key type</typeparam>
+        /// <typeparam name="V">value type</typeparam>
+        /// <param name="key">single key</param>
+        /// <param name="value">single value</param>
+        /// <returns>A read-only dictionary with the one specified <paramref name="key"/> and <paramref name="value"/>.</returns>
+        public static IReadOnlyDictionary<K, V> Singleton<K, V>(K key, V value, IEqualityComparer<K>? keyComparer = null) where K: notnull =>
+            new Singletons.ReadOnlySingletonDictionary<K, V>(key, value, keyComparer);
 
     }
 
